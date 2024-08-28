7be85e9ed96d86a1ff2d86c8968fa7a2beaa03a8
DeGiovine <andrea.degiovine@finconsgroup.com>
2024-08-28 16:23:00 +0200

    catch api exceptions

--- custom_components/stellantis_vehicles/__init__.py
+++ custom_components/stellantis_vehicles/__init__.py
@@ -22,18 +22,23 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config.entry_id] = stellantis
 
-    vehicles = await stellantis.get_user_vehicles()
+    try:
+        vehicles = await stellantis.get_user_vehicles()
+    except Exception as e:
+        _LOGGER.error(str(e))
+        vehicles = {}
 
     for vehicle in vehicles:
         coordinator = await stellantis.async_get_coordinator(vehicle)
         await coordinator.async_config_entry_first_refresh()
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config, platform)
-        )
+    if vehicles:
+        for platform in PLATFORMS:
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config, platform)
+            )
 
-    stellantis.register_webhook()
+        stellantis.register_webhook()
 
     return True
 
