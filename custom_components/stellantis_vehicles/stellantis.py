5863c6d8ca6d8ca61f3b0349ab5af7be10c00f39
Dirk Moeller <MoellerDi@users.noreply.github.com>
2025-07-29 19:31:23 +0000

    Improves code quality: fix string statement has no effect

--- custom_components/stellantis_vehicles/stellantis.py
+++ custom_components/stellantis_vehicles/stellantis.py
@@ -176,10 +176,10 @@ class StellantisBase:
         return asyncio.run_coroutine_threadsafe(async_func, self._hass.loop).result()
 
     async def hass_notify(self, translation_key):
+        """Create a persistent notification."""
         if hasattr(self, '_entry') and not self.get_stored_config("switch_notifications"):
             return
 
-        """Create a persistent notification."""
         translations = await translation.async_get_translations(self._hass, self._hass.config.language, "common", {DOMAIN})
         notification_title = "Stellantis Vehicles"
         if translations.get(f"component.stellantis_vehicles.common.{translation_key}_title", None):
