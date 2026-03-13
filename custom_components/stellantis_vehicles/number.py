bf7cec329e407f1d449d38ec5ac43142da9313bd
csd5494 <csd5494@csd.uoc.gr>
2026-03-13 17:54:45 +0200

    Feature implementation - track charge cost and real-world consumption

--- custom_components/stellantis_vehicles/number.py
+++ custom_components/stellantis_vehicles/number.py
@@ -1,7 +1,7 @@
 import logging
 
 from homeassistant.core import HomeAssistant
-from homeassistant.const import ( PERCENTAGE, UnitOfTime )
+from homeassistant.const import ( PERCENTAGE, UnitOfTime, UnitOfEnergy )
 from homeassistant.components.number import NumberMode, NumberEntityDescription
 from homeassistant.const import EntityCategory
 
@@ -39,6 +39,19 @@ async def async_setup_entry(hass:HomeAssistant, entry, async_add_entities) -> No
             )
             entities.extend([StellantisBaseNumber(coordinator, description)])
 
+        if coordinator.vehicle_type in [VEHICLE_TYPE_ELECTRIC, VEHICLE_TYPE_HYBRID]:
+            description = NumberEntityDescription(
+                name = "kwh_cost",
+                key = "kwh_cost",
+                translation_key = "kwh_cost",
+                icon = "mdi:cash",
+                native_min_value = 0,
+                native_step = 0.001,
+                mode = NumberMode.BOX,
+                entity_category = EntityCategory.CONFIG
+            )
+            entities.extend([StellantisKwhCostNumber(coordinator, description, 0)])
+
         description = NumberEntityDescription(
             name = "refresh_interval",
             key = "refresh_interval",
@@ -54,3 +67,10 @@ async def async_setup_entry(hass:HomeAssistant, entry, async_add_entities) -> No
         entities.extend([StellantisBaseNumber(coordinator, description, UPDATE_INTERVAL)])
 
     async_add_entities(entities)
+
+
+class StellantisKwhCostNumber(StellantisBaseNumber):
+    @property
+    def native_unit_of_measurement(self):
+        """ Native value unit of measurement. """
+        return f"{self._coordinator.currency_code}/{UnitOfEnergy.KILO_WATT_HOUR}"
