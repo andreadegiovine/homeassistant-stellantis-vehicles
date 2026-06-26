5a20630b8e993ce3f4a2c6121d6480b01b857179
Jost Salathé <34694583+jostsalathe@users.noreply.github.com>
2026-06-26 12:20:47 +0200

    changed _async_update_data() so that it also accepts new data sets without top level 'createdAt'

--- custom_components/stellantis_vehicles/base.py
+++ custom_components/stellantis_vehicles/base.py
@@ -59,20 +59,19 @@ class StellantisVehicleCoordinator(DataUpdateCoordinator):
         try:
             # Vehicle status
             new_data = await self._stellantis.get_vehicle_status(self._vehicle)
-            if "createdAt" in new_data:
-                if "createdAt" in self._data:
-                    old_data_time = datetime.fromisoformat(self._data["createdAt"])
-                    new_data_time = datetime.fromisoformat(new_data["createdAt"])
-                    if new_data_time > old_data_time:
-                        # Only accept new data sets that are actually newer than the last one
-                        self._data = new_data
-                        accepted_new_data = True
-                    elif new_data_time < old_data_time:
-                        # If the received data set is actually older, log a warning
-                        _LOGGER.warning(f"Discarded stale data set - new 'createdAt' too old: self._data: {old_data_time}, new_data: {new_data_time}")
-                else:
+            if "createdAt" in new_data and "createdAt" in self._data:
+                old_data_time = datetime.fromisoformat(self._data["createdAt"])
+                new_data_time = datetime.fromisoformat(new_data["createdAt"])
+                if new_data_time > old_data_time:
+                    # Only accept new data sets that are actually newer than the last one
                     self._data = new_data
                     accepted_new_data = True
+                elif new_data_time < old_data_time:
+                    # If the received data set is actually older, log a warning
+                    _LOGGER.warning(f"Discarded stale data set - new 'createdAt' too old: self._data: {old_data_time}, new_data: {new_data_time}")
+            else:
+                self._data = new_data
+                accepted_new_data = True
         except ConfigEntryAuthFailed:
             _LOGGER.debug("---------- END _async_update_data")
             raise
